aafcd597ce6dcb0dc6d0f49eac4bf6a88ab8bb8b
npichuzhkin <ya.pichuzhkin@yqndex.ru>
2024-09-24 19:35:11 +0400

    generate project with Spring Initializr and db configuration

---
+++ src/main/java/com/projectsfortraining/SensorRESTProject/SensorRestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.projectsfortraining.SensorRESTProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorRestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectsfortraining/SensorRESTProject/SensorRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsfortraining.SensorRESTProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
